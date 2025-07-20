bf0323fcab8d2f302efec130aa3cc96c951896ef
Charles Qian <charles.qian@gmail.com>
2025-07-19 21:53:50 -0400

    Gradients are cool

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import StockTableLoader from './components/StockTableLoader';
 
 export default async function HomePage() {
   return (
-    <main className="min-h-screen bg-gray-900 text-white p-4">
+    <main className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 text-white p-4">
       <h1 className="text-4xl font-bold text-center mb-8 text-emerald-400">
-      <img
+        <img
           src="/stock.svg"
-          alt="Stock Screener Icon"
-          className="inline-block mr-3 my-2" // Added my-2 for 0.5rem margin top and bottom
+          className="inline-block mr-3 my-2"
         />
+
       </h1>
       <Suspense fallback={<Spinner />}>
         <StockTableLoader />
