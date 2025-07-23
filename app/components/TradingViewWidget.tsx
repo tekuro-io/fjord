300a89a44fa0f2f13dff6e7ecf1959e49e0e2bd2
Charles Qian <charles.qian@gmail.com>
2025-07-23 03:08:54 -0400

    added market ticker

---
+++ app/components/TradingViewWidget.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useEffect, useRef, memo } from "react";
+
+const TradingViewWidget: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const script = document.createElement("script");
+    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-tickers.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = JSON.stringify({
+      symbols: [
+        { proName: "FOREXCOM:SPXUSD", title: "S&P 500 Index" },
+        { proName: "NASDAQ:PLTR", title: "Palantir" },
+        { proName: "NASDAQ:AVGO", title: "Broadcom" },
+        { proName: "TRADENATION:SOLANA", title: "Solana" }
+      ],
+      colorTheme: "dark",
+      locale: "en",
+      largeChartUrl: "",
+      isTransparent: true,
+      showSymbolLogo: false
+    });
+
+    containerRef.current.appendChild(script);
+  }, []);
+
+  return (
+    <div className="tradingview-widget-container" ref={containerRef}>
+      <div className="tradingview-widget-container__widget" />
+      <div className="tradingview-widget-copyright">
+        <a
+          href="https://www.tradingview.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+      
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default memo(TradingViewWidget);
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,13 @@ import StockTableLoader from './components/StockTableLoader';
 import Image from 'next/image'
 import Spinner from './components/Spinner';
 import { CommitLink } from './components/CommitLink';
+import dynamic from 'next/dynamic';
+
+
+const TradingViewWidget = dynamic(
+    () => import('./components/TradingViewWidget'),
+    { ssr: false }
+  );
 
 export default async function HomePage() {
     return (
@@ -16,6 +23,7 @@ export default async function HomePage() {
                     className='inline-block mr-3 my-2'
                 />
             </h1>
+            <TradingViewWidget />
             <Suspense fallback={<Spinner />}>
                 <StockTableLoader />
             </Suspense>
