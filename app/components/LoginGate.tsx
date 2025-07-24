eaeef0bf37aa66f99e169c2b844ccf475d581b11
Charles Qian <charles.qian@gmail.com>
2025-07-24 17:19:17 -0400

    password look

--- app/components/LoginGate.tsx
+++ app/components/LoginGate.tsx
@@ -34,9 +34,18 @@ export default function LoginGate({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex items-start justify-center px-4 text-white pt-32">
       <div className="bg-gray-800 p-8 rounded-md w-full max-w-sm shadow-lg">
-        <h1 className="text-2xl font-bold mb-4 text-blue-400 text-center">
-          Login
-        </h1>
+
+      <div className="bg-gray-700 py-3 px-6 rounded-t-lg flex items-center justify-between">
+        <div className="flex items-center gap-3">
+
+            <h1 className="text-2xl font-bold mb-4 text-blue-400 text-center">
+            Login
+            </h1>
+          <h2 className="text-xl font-bold text-white">Momentum Scanner</h2>
+        </div>
+
+
+
         <input
           type="password"
           placeholder="Enter password"
